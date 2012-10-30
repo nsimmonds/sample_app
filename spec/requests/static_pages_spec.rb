9d9018541cfc5be1c8fa9df9b66f821737746316
Nick Simmonds <nick@nicksimmonds.com>
2012-10-30 18:58:51 -0400

    Finished adding layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,19 +6,26 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all these" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector('title', text: full_title(page_title))   }
+  end
+
   describe "Home page" do
     before { visit root_path }
+    let(:heading) {'Sample App'}
+    let(:page_title) {''}
 
-    it { should have_selector('h1',    text: 'Sample App') }
-    it { should have_selector('title', text: full_title('')) }
+    it_should_behave_like "all these"
     it { should_not have_selector 'title', text: '| Home' }
   end
 
   describe "Help page" do
     before { visit help_path }
+    let(:heading) {'Help'}
+    let(:page_title) {'Help'}
+    it_should_behave_like "all these"
 
-    it { should have_selector('h1',    text: 'Help') }
-    it { should have_selector('title', text: full_title('Help')) }
   end
 
   describe "About page" do
